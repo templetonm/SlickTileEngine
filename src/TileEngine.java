0bff6f856b1056d15a2ad3f35c0ff2ceb4e44168
Robert Sikorski <sikorski.rob@gmail.com>
2011-10-15 09:54:22 -0400

    Character sprite is able to move toward edge of map before map scrolls.

--- src/TileEngine.java
+++ src/TileEngine.java
@@ -11,12 +11,17 @@ import org.newdawn.slick.tiled.TiledMap;
 public class TileEngine extends BasicGame {
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 600;
+	private static final int PLAYERPOSITIONOFFYUP = 200;
+	private static final int PLAYERPOSITIONOFFYDOWN = 400;
+	private static final int PLAYERPOSITIONOFFXLEFT = 200;
+	private static final int PLAYERPOSITIONOFFXRIGHT = 600;
 	// Player speed
 	private static final int SPEED = 6;
 	private TiledMap map;
 	private Image player;
 	private int offX = 0;
 	private int offY = 0;
+	private int [] playerPosition = null;
 	
 	public TileEngine() {
 		super("TileEngine");
@@ -30,14 +35,61 @@ public class TileEngine extends BasicGame {
 		map = new TiledMap("resources/demo_map1.tmx");
 		// TODO: Use animations and spritesheets
 		player = new Image("resources/player.png");
+
+		// Initial position is center of screen.
+		playerPosition = new int[] {WIDTH/2-player.getWidth()/2, HEIGHT/2-player.getHeight()/2};
 	}
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
-		if (container.getInput().isKeyDown(Input.KEY_UP)) { offY += SPEED; }
-		if (container.getInput().isKeyDown(Input.KEY_DOWN)) { offY -= SPEED; }
-		if (container.getInput().isKeyDown(Input.KEY_LEFT)) { offX += SPEED; }
-		if (container.getInput().isKeyDown(Input.KEY_RIGHT)) { offX -= SPEED; }
+		if (container.getInput().isKeyDown(Input.KEY_UP))
+		{
+			if (playerPosition[1] < PLAYERPOSITIONOFFYUP)
+			{
+				offY += SPEED;
+			}
+			else
+			{
+				playerPosition[1] -= SPEED;
+			}
+		}
+		
+		if (container.getInput().isKeyDown(Input.KEY_DOWN))
+		{
+			if (playerPosition[1] > PLAYERPOSITIONOFFYDOWN)
+			{
+				offY -= SPEED;
+			}
+			else
+			{
+				playerPosition[1] += SPEED;
+			}
+		}
+		
+		if (container.getInput().isKeyDown(Input.KEY_LEFT))
+		{
+			if (playerPosition[0] < PLAYERPOSITIONOFFXLEFT)
+			{
+				offX += SPEED;
+			}
+			else
+			{
+				playerPosition[0] -= SPEED;
+			}
+		}
+		
+		if (container.getInput().isKeyDown(Input.KEY_RIGHT))
+		{
+			if (playerPosition[0] > PLAYERPOSITIONOFFXRIGHT)
+			{
+				offX -= SPEED;
+			}
+			else
+			{
+				playerPosition[0] += SPEED;
+			}
+		}
+		
 		if (container.getInput().isKeyDown(Input.KEY_ESCAPE)) { container.exit(); }
 	}
 
@@ -45,7 +97,7 @@ public class TileEngine extends BasicGame {
 	public void render(GameContainer container, Graphics g) throws SlickException {
 		map.render(offX, offY);
 		// Draw the player in the center of the screen		
-		player.draw(WIDTH/2-player.getWidth()/2,HEIGHT/2-player.getHeight()/2);
+		player.draw(playerPosition[0],playerPosition[1]);
 	}
 	
 	public static void main(String[] args) 
